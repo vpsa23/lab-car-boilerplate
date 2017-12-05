1dcf4b19823f4ffccabbad4ab596e61b5d9b1f49
vpsa23 <vpsa23@hotmail.com>
2017-12-05 20:44:11 -0300

    version movil

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 // La funcionalidad de tu proyecto
+function initMap() {
+  var chile = {lat: -33.45, lng: -70.6667};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: chile
+ });
+        
+  var marker = new google.maps.Marker({
+    position: chile,
+    map: map
+	});
+}
